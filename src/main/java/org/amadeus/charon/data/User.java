6eb9fce1f77a81422ee12b02a17556a16a8555e3
Xuwen Nie <pandanie1106@gmail.com>
2017-02-24 15:25:00 -0400

    Userlogin

--- src/main/java/org/amadeus/charon/data/User.java
+++ src/main/java/org/amadeus/charon/data/User.java
@@ -1,5 +1,26 @@
 package org.amadeus.charon.data;
 
 public class User {
+	private String Username;
+	private String Userpassword;
+	private int admin;
 
+public String getUsername() {
+	return Username;
+}
+public void setUsername(String username) {
+	Username = username;
+}
+public String getUserpassword() {
+	return Userpassword;
+}
+public void setUserpassword(String userpassword) {
+	Userpassword = userpassword;
+}
+public void setadmin(int i){
+	admin = i;
+}
+public int getadmin(){
+	return admin;
+}
 }
--- src/main/java/org/amadeus/charon/data/UserManager.java
+++ src/main/java/org/amadeus/charon/data/UserManager.java
@@ -1,12 +1,39 @@
 package org.amadeus.charon.data;
+import java.util.ArrayList;
 
 public class UserManager {
-
+ArrayList <User> Users = new ArrayList <User> ();
+User user = new User();
   private static UserManager instance;
   
-  private UserManager() {
-  
+  UserManager() {
+
   }
+  public void UserManagerAdd(String name,String password){
+	  User user = new User();
+	  user.setUsername(name);
+	  user.setUserpassword(password);
+	  Users.add(user);
+  }
+  private void UserManagerDelete(String name,String password){
+	  
+  }
+  public int isEmpty(User user){
+	  if(user.getUsername().equals("")&&user.getUserpassword().equals("")){
+		  return 1;
+	  }
+	  return 0;
+  }
+ public int isCorrect(User user){
+	 for(int i=0;i<Users.size();i++){
+		 if(user.getUsername().equals(Users.get(i).getUsername())&&user.getUserpassword().equals(Users.get(i).getUserpassword())){
+			 return 1;
+		 }
+	 }
+
+	 return 0;
+ }
+  
   
   public static UserManager getInstance() {
     if (instance == null) {
